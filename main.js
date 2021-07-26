009c2724fa5b3f7dd280b269debdd30d4ddc6dbd
Strombreaker07 <80044297+Strombreaker07@users.noreply.github.com>
2021-07-26 18:48:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+//CocoSsd (Coco Single Shot MultiBox Detection)
+img = "";
+
+function preload()
+{
+    img = loadImage("suga.jpeg");
+} 
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("Human", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    rect(30, 60, 450, 350)
+}
